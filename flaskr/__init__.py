18c1e4e252e844996de65f0b819fad6872871ef0
jbhatt31 <jay.bhatt@gtri.gatech.edu>
2023-04-26 14:40:23 -0400

    (Version 2.0.0) Substantially changed __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,41 +1,80 @@
-import os
-
+"""Holds the application factory and its derivative functions."""
 from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+# Initializes the SQLAlchemy object
+db = SQLAlchemy()
 
 
 def create_app(test_config=None):
-    """Create and configure an instance of the Flask application."""
+    """Creates and configures an instance of the Flask application.
+
+    :param test_config: The test config object.
+    :return: The Flask app.
+    """
+
+    # Constructs the Flask app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        # a default secret that should be overridden by instance config
-        SECRET_KEY='dev',
-        # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        # Loads the instance config object when not testing
+        app.config.from_object("config.DevelopmentConfig")
     else:
-        # load the test config if passed in
+        # Loads the test config object if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # register the database commands
-    from . import db
+    # Initializes the Flask app with the SQLAlchemy object
     db.init_app(app)
 
-    # apply the blueprints to the app
-    from . import auth, blog
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(blog.bp)
+    # Registers the blueprints
+    register_blueprints(app)
 
-    # make url_for('index') == url_for('blog.index')
-    app.add_url_rule('/', endpoint='index')
+    # Prepares the database and login functionality
+    prepare_database_and_login(app)
 
     return app
+
+
+def register_blueprints(app):
+    """Registers the blueprints of the flaskr package.
+
+    :param app: The Flask app.
+    """
+
+    # Imports the blueprint modules from the current package
+    from . import auth, blog
+
+    # Registers the blueprints from the above modules
+    app.register_blueprint(auth.bp, url_prefix="/")
+    app.register_blueprint(blog.bp, url_prefix="/")
+
+
+def prepare_database_and_login(app):
+    """Prepares the database and login manager.
+
+    :param app: The Flask app.
+    """
+
+    # Imports all database models to ensure db.create_all() is going to be
+    # called properly
+    from .models import Post, Upvote, User
+
+    # Constructs the app context and database, respectively
+    with app.app_context():
+        db.create_all()
+
+    # Points the LoginManager instance to the Login Page
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        """Locates the current user of the session by his/her ID.
+
+        :param user_id: The User ID.
+        :return: The current user.
+        """
+
+        return User.query.get(int(user_id))
