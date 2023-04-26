2e051f8436b067f748a810c762d788f7a1eb7213
jbhatt31 <jay.bhatt@gtri.gatech.edu>
2023-04-26 15:43:10 -0400

    (Version 2.0.0) Created config.py

---
+++ config.py
@@ -0,0 +1,43 @@
+"""Holds the configuration objects."""
+from datetime import timedelta
+from os import environ, path
+
+from dotenv import load_dotenv
+
+# Obtains this file's module's path
+project_root_dir = path.abspath(path.dirname(__file__))
+
+# Loads .env environment variables
+load_dotenv(path.join(project_root_dir, ".env"))
+
+
+class BaseConfig:
+    """Sets the default Flask configuration variables."""
+
+    # Default Configurations
+    SECRET_KEY = environ.get("SECRET_KEY", "Default Secret Key")
+    SESSION_COOKIE_NAME = environ.get(
+        "SESSION_COOKIE_NAME", "Default Session Cookie Name"
+    )
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=5)
+
+    # Database Configurations
+    DATABASE_NAME = environ.get("DATABASE_NAME", "default.db")
+    SQLALCHEMY_DATABASE_URI = environ.get(
+        "SQLALCHEMY_DATABASE_URI", f"sqlite:///${DATABASE_NAME}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Optimizes the Flask configuration variables for a development
+    environment."""
+
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    """Optimizes the Flask configuration variables for a production
+    environment."""
+
+    TESTING = False
