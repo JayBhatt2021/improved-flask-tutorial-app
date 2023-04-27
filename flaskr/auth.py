c8ab9b7047e33ac36d58ccc6fd8159d5b8a64a24
jbhatt31 <jay.bhatt@gtri.gatech.edu>
2023-04-27 12:55:17 -0400

    (Version 2.0.0) Substantially changed auth.py

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,112 +1,112 @@
-import functools
-
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
+"""Holds the auth blueprint."""
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import get_db
+from flaskr import db
+from flaskr.models import User
 
-# 'auth' corresponds to the Blueprint's name
-# '__name__' helps locate the root path of the Blueprint
-# '/auth' is prepended to all the URLs associated with this Blueprint
-bp = Blueprint('auth', __name__, url_prefix='/auth')
-
-
-def login_required(view):
-    """View decorator that redirects anonymous users to the login page."""
-
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login'))
-
-        return view(**kwargs)
-
-    return wrapped_view
+# The blueprint name is "auth".
+# The package that this blueprint is located in is "__name__".
+bp = Blueprint("auth", __name__)
 
 
-@bp.before_app_request
-def load_logged_in_user():
-    """If a user id is stored in the session, load the user object from
-    the database into ``g.user``."""
-    user_id = session.get('user_id')
-
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?;', (user_id,)
-        ).fetchone()
-
-
-@bp.route('/register', methods=('GET', 'POST'))
+@bp.route("/register", methods=("GET", "POST"))
 def register():
-    """Register a new user.
+    """Registers the user if a valid username and password are inputted.
 
-    Validates that the username is not already taken. Hashes the
-    password for security."""
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
+    :return: The rendered HTML of the Login Template if all inputs are valid;
+             otherwise, the rendered HTML of the Register Template is returned.
+    """
+
+    if request.method == "POST":
+        # Gets the inputted username and password from the Register Template
+        username = request.form["username"]
+        password = request.form["password"]
+
+        # Initializes the error-checking variables
         error = None
+        username_exists = User.query.filter_by(username=username).first()
 
+        # Assigns the appropriate message to error (if needed)
         if not username:
-            error = 'Username is required.'
+            error = "Username is required."
         elif not password:
-            error = 'Password is required.'
+            error = "Password is required."
+        elif username_exists:
+            error = "This username already exists."
 
+        # Registers the new user and redirects him/her to the Login Page if
+        # error is None
         if error is None:
-            try:
-                db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?);",
-                    (username, generate_password_hash(password)),
-                )
-                db.commit()
-            except db.IntegrityError:
-                # The username was already taken, which caused the
-                # commit to fail. Show a validation error.
-                error = f"User {username} is already registered."
-            else:
-                # Success, go to the login page.
-                return redirect(url_for("auth.login"))
+            new_user = User(
+                username=username,
+                password=generate_password_hash(password, method="sha256"),
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash("You have been registered!", category="success")
 
-        flash(error)
+            return redirect(url_for("auth.login"))
 
-    return render_template('auth/register.html')
+        # Flashes an error message if error is not None
+        flash(error, category="error")
+
+    return render_template("auth/register.html", user=current_user)
 
 
-@bp.route('/login', methods=('GET', 'POST'))
+@bp.route("/login", methods=("GET", "POST"))
 def login():
-    """Log in a registered user by adding the user id to the session."""
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
+    """Logs in the user if the correct username and password are inputted.
+
+    :return: The rendered HTML of the signed-in version of the Index Template
+             if all inputs are valid; otherwise, the rendered HTML of the Login
+             Template is returned.
+    """
+
+    if request.method == "POST":
+        # Gets the inputted username and password from the Login Template
+        username = request.form["username"]
+        password = request.form["password"]
+
+        # Queries the database for the user by the inputted username
+        user = User.query.filter_by(username=username).first()
+
+        # Initializes the error-checking variables
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+        wrong_password = not check_password_hash(user.password, password)
 
+        # Assigns the appropriate message to error (if needed)
         if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
+            error = "This username does not exist."
+        elif wrong_password:
+            error = "This password is incorrect."
 
+        # Log ins the user if the correct username and password are inputted
         if error is None:
-            # store the user id in a new session and return to the index
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            login_user(user, remember=True)
+            flash("You have been logged in!", category="success")
 
-        flash(error)
+            return redirect(url_for("index"))
 
-    return render_template('auth/login.html')
+        # Flashes an error message if error is not None
+        flash(error, category="error")
+
+    return render_template("auth/login.html", user=current_user)
 
 
-@bp.route('/logout')
+@bp.route("/logout")
+@login_required
 def logout():
-    """Clear the current session, including the stored user id."""
-    session.clear()
-    return redirect(url_for('index'))
+    """Logs out the user to the unsigned-in version of the Index Template if
+    he/she was already logged-in.
+
+    :return: The rendered HTML of the unsigned-in version of the Index
+             Template.
+    """
+
+    logout_user()
+    flash("You have been logged out!", category="success")
+
+    return redirect(url_for("index"))
